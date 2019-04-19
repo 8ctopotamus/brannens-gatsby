fd05fe7775acbc46e367cd533d1d9fd3604caaf6
8ctopotamus <joshnaylor88@gmail.com>
2019-04-19 16:59:35 -0500

    Adds react-image-gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HomeLogos from '../components/home-logos'
+import Slider from '../components/Slider'
 
 const IndexPage = () => (
   <Layout>
@@ -12,6 +13,7 @@ const IndexPage = () => (
         `tools`,
       ]}
     />
+    <Slider />
     <h1>Top Products</h1>
     <div className="page-content">
       <HomeLogos />
